b80b1e11077824c576eaba3f36528a48bac90e8f
Bread For The People <bread.for.the.people@planet.earth>
2020-09-05 14:18:40 -0400

    Bitcoin flour for your bread!

---
+++ contracts/strategies/curve/CRVStrategyRENBTCMainnet.sol
@@ -0,0 +1,44 @@
+pragma solidity 0.5.16;
+import "./CRVStrategyWRenBTC.sol";
+import "./PriceConvertor.sol";
+
+/**
+* Adds the mainnet addresses to the CRVStrategyWBTC
+*/
+contract CRVStrategyRENBTCMainnet is CRVStrategyWRenBTC {
+
+  // token addresses
+  address constant public __renbtc = address(0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D);
+  address constant public __wbtcMix = address(0x49849C98ae39Fff122806C06791Fa73784FB3675);
+  address constant public __crv = address(0xD533a949740bb3306d119CC777fa900bA034cd52);
+  address constant public __weth = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+  address constant public __gauge = address(0xB1F2cdeC61db658F091671F5f199635aEF202CAC);
+  address constant public __mintr = address(0xd061D61a4d941c39E5453435B6345Dc261C2fcE0); // _mintr
+
+  // protocols
+  address constant public __curve = address(0x93054188d876f558f4a66B2EF1d97d16eDf0895B);
+  address constant public __uniswap = address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+
+  uint256 constant public __tokenIndex = 0;
+
+  constructor(
+    address _storage,
+    address _vault
+  )
+  CRVStrategyWRenBTC(
+    _storage,
+    __renbtc,
+    _vault,
+    __tokenIndex, // token index for WBTC
+    __wbtcMix,
+    __curve, // curve protocol's pool for WBTC
+    __crv, // the CRV token address
+    __weth,
+    __gauge,
+    __mintr,
+    __uniswap // uniswap
+  )
+  public {
+    wbtcPriceCheckpoint = wbtcValueFromMixToken(mixTokenUnit);
+  }
+}
---
+++ test/mainnet-fork-test-curve-wbtc.js
@@ -0,0 +1,136 @@
+// This test is only invoked if MAINNET_FORK is set
+if (process.env.MAINNET_FORK) {
+  const Utils = require("./Utils.js");
+  const MFC = require("./mainnet-fork-test-config.js");
+  const { expectRevert, send } = require("@openzeppelin/test-helpers");
+  const BigNumber = require("bignumber.js");
+  const Controller = artifacts.require("Controller");
+  const Vault = artifacts.require("Vault");
+  const Storage = artifacts.require("Storage");
+  const CRVStrategyWBTC = artifacts.require("CRVStrategyWBTCMainnet");
+  const PriceConvertor = artifacts.require("PriceConvertor");
+  const FeeRewardForwarder = artifacts.require("FeeRewardForwarder");
+
+  // ERC20 interface
+  const IERC20 = artifacts.require("IERC20");
+
+  // UniswapV2 Router (can we ignore this or not?)
+  const UniswapV2Router02 = artifacts.require("UniswapV2Router02");
+
+  BigNumber.config({ DECIMAL_PLACES: 0 });
+
+  contract("Mainnet Curve WBTC", function (accounts) {
+    describe("Curve savings", function () {
+      // external contracts
+      let wbtc;
+      let ycrv;
+
+      // external setup
+      let wbtcWhale = MFC.WBTC_WHALE_ADDRESS;
+
+      // parties in the protocol
+      let governance = accounts[1];
+      let rewardCollector = accounts[2];
+      let farmer1 = accounts[3];
+      let farmer2 = accounts[4];
+
+      // numbers used in tests
+      const farmerBalance = "10" + "00000000";
+
+      // only used for ether distribution
+      let etherGiver = accounts[9];
+
+      // Core protocol contracts
+      let storage;
+      let controller;
+      let wbtcVault;
+      let daiStrategy;
+      let ycrvStrategy;
+
+      // secondary protocol contracts
+
+      async function setupExternalContracts() {
+        ycrv = await IERC20.at(MFC.YCRV_ADDRESS);
+        wbtc = await IERC20.at(MFC.WBTC_ADDRESS);
+        let crv = MFC.CRV_ADDRESS;
+      }
+
+      async function resetDaiBalance() {
+        // Give whale some ether to make sure the following actions are good
+        await send.ether(etherGiver, wbtcWhale, "1" + "000000000000000000");
+        // reset token balance
+        await wbtc.transfer(wbtcWhale, await wbtc.balanceOf(farmer1), {
+          from: farmer1,
+        });
+        await wbtc.transfer(wbtcWhale, await wbtc.balanceOf(farmer2), {
+          from: farmer2,
+        });
+        await wbtc.transfer(farmer1, farmerBalance, { from: wbtcWhale });
+        await wbtc.transfer(farmer2, farmerBalance, { from: wbtcWhale });
+        assert.equal(farmerBalance, await wbtc.balanceOf(farmer1));
+      }
+
+      async function setupCoreProtocol() {
+        // deploy storage
+        storage = await Storage.new({ from: governance });
+
+        feeRewardForwarder = await FeeRewardForwarder.new(storage.address, MFC.UNISWAP_V2_ROUTER02_ADDRESS, { from: governance });
+
+        // set up controller
+        controller = await Controller.new(storage.address, feeRewardForwarder.address, {
+          from: governance,
+        });
+        assert.equal(await controller.governance(), governance);
+
+        await storage.setController(controller.address, { from: governance });
+
+        // set up the wbtcVault with 90% investment
+        wbtcVault = await Vault.new(storage.address, wbtc.address, 90, 100, {
+          from: governance,
+        });
+
+        // set up the strategies
+        wbtcStrategy = await CRVStrategyWBTC.new(
+          storage.address,
+          wbtcVault.address,
+          { from: governance }
+        );
+
+        // link vaults with strategies
+        await controller.addVaultAndStrategy(
+          wbtcVault.address,
+          wbtcStrategy.address,
+          { from: governance }
+        );
+      }
+
+      beforeEach(async function () {
+        await setupExternalContracts();
+        await setupCoreProtocol();
+        await resetDaiBalance();
+      });
+
+      async function depositVault(_farmer, _underlying, _vault, _amount) {
+        await _underlying.approve(_vault.address, _amount, { from: _farmer });
+        await _vault.deposit(_amount, { from: _farmer });
+        assert.equal(_amount, await _vault.balanceOf(_farmer));
+        assert.equal(_amount, await _vault.getContributions(_farmer));
+      }
+
+      it("A farmer investing wbtc", async function () {
+        let farmerOldBalance = new BigNumber(await wbtc.balanceOf(farmer1));
+        await depositVault(farmer1, wbtc, wbtcVault, farmerBalance);
+        let hours = 12;
+        for (let i = 0; i < hours; i++) {
+          let blocksPerHour = 240;
+          await Utils.advanceNBlock(blocksPerHour);
+          await controller.doHardWork(wbtcVault.address, { from: governance });
+          await controller.doHardWork(wbtcVault.address, { from: governance });
+        }
+        await wbtcVault.withdraw(farmerBalance, { from: farmer1 });
+        let farmerNewBalance = new BigNumber(await wbtc.balanceOf(farmer1));
+        Utils.assertBNGt(farmerNewBalance, farmerOldBalance);
+      });
+    });
+  });
+}
