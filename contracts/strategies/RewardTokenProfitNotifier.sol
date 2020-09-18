a35011798d75b03c61c1565fb923730d62fb0984
Bread For The People <bread.for.the.people@planet.earth>
2020-09-17 20:19:13 -0400

    Everything is bread. Bread is UNI! #BreadForThePeople!

---
+++ contracts/strategies/RewardTokenProfitNotifier.sol
@@ -0,0 +1,46 @@
+pragma solidity 0.5.16;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "../hardworkInterface/IController.sol";
+import "../Controllable.sol";
+
+contract RewardTokenProfitNotifier is Controllable {
+  using SafeMath for uint256;
+  using SafeERC20 for IERC20;
+
+  uint256 public profitSharingNumerator;
+  uint256 public profitSharingDenominator;
+  address public rewardToken;
+
+  constructor(
+    address _storage,
+    address _rewardToken
+  ) public Controllable(_storage){
+    rewardToken = _rewardToken;
+    // persist in the state for immutability of the fee
+    profitSharingNumerator = 5; //IController(controller()).profitSharingNumerator();
+    profitSharingDenominator = 100; //IController(controller()).profitSharingDenominator();
+    require(profitSharingNumerator < profitSharingDenominator, "invalid profit share");
+  }
+
+  event ProfitLogInReward(uint256 profitAmount, uint256 feeAmount, uint256 timestamp);
+
+  function notifyProfitInRewardToken(uint256 _rewardBalance) internal {
+    if( _rewardBalance > 0 ){
+      uint256 feeAmount = _rewardBalance.mul(profitSharingNumerator).div(profitSharingDenominator);
+      emit ProfitLogInReward(_rewardBalance, feeAmount, block.timestamp);
+      IERC20(rewardToken).safeApprove(controller(), 0);
+      IERC20(rewardToken).safeApprove(controller(), feeAmount);
+      
+      IController(controller()).notifyFee(
+        rewardToken,
+        feeAmount
+      );
+    } else {
+      emit ProfitLogInReward(0, 0, block.timestamp);
+    }
+  }
+
+}
