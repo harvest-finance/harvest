389d6fb4656acc8dfa44253455c686440d4abba3
Byron-McKeeby <byron.mckeeby@protonmail.com>
2020-09-24 15:37:16 -0700

    2 spaces + use Storage.sol

--- contracts/proxies/GovernableProxy.sol
+++ contracts/proxies/GovernableProxy.sol
@@ -1,84 +1,65 @@
 pragma solidity 0.5.16;
 
-
+import "../Storage.sol";
 import "./VendoredOZ.sol";
 
 // based on
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/proxy/TransparentUpgradeableProxy.sol
 contract GovernableProxy is UpgradeableProxy {
 
-    bytes32 private constant _ADMIN_SLOT = 0x98a014adb3d21d2b11e570ae3fc91fc10180b682ca5ae9f0d7e5f36837d852cd;
-    bytes32 private constant _GOVERNANCE_SLOT = 0xe490bd502202d301ec2f5c6cf31aba1dccecc9bf9ff4d9dc3adf79ecd28da2f7;
+  bytes32 private constant _STORAGE_SLOT = 0x98a014adb3d21d2b11e570ae3fc91fc10180b682ca5ae9f0d7e5f36837d852cd;
 
-    event AdminChanged(address previousAdmin, address newAdmin);
+  event StorageChanged(address previousAdmin, address newAdmin);
 
-    constructor(address _logic, bytes memory _data, address _admin, address _governance)
-    UpgradeableProxy(_logic, _data)
-    public
-    {
-        assert(_ADMIN_SLOT == bytes32(uint256(keccak256("eip1967.proxy.Bread for the People")) - 1));
-        assert(_GOVERNANCE_SLOT == bytes32(uint256(keccak256("eip1967.proxy.Panem et Circenses")) - 1));
-        _setAdmin(_admin);
-        _setGovernance(_governance);
+  constructor(address _logic, bytes memory _data, address _storage)
+  UpgradeableProxy(_logic, _data)
+  public
+  {
+    assert(_STORAGE_SLOT == bytes32(uint256(keccak256("eip1967.proxy.Bread for the People")) - 1));
+    _setStorage(_storage);
+  }
+
+  function _isAdmin(address _someone) internal view returns (bool) {
+    return _storage.isGovernance(_someone);
+  }
+
+  modifier ifAdmin() {
+    if (_isAdmin(msg.sender)) {
+      _;
+    } else {
+      _fallback();
     }
+  }
 
-    function _isAdmin(address _someone) internal view returns (bool) {
-        return _someone == _admin() || _someone == _governance();
+  function changeStorage(address newStorage) external ifAdmin {
+    require(newStorage != address(0), "TransparentUpgradeableProxy: new admin is the zero address");
+    emit StorageChanged(_storage(), newStorage);
+    _setStorage(newStorage);
+  }
+
+  function _storage() internal view returns (Storage sto) {
+    bytes32 slot = _STORAGE_SLOT;
+    // solhint-disable-next-line no-inline-assembly
+    assembly {
+      sto := sload(slot)
     }
+  }
 
-    modifier ifAdmin() {
-        if (_isAdmin(msg.sender)) {
-            _;
-        } else {
-            _fallback();
-        }
+  function _governance() internal view returns (address) {
+    return _storage.governance();
+  }
+
+  function _beforeFallback() internal {
+    super._beforeFallback();
+  }
+
+  function _setStorage(address newStorage) private {
+    bytes32 slot = _STORAGE_SLOT;
+
+    // solhint-disable-next-line no-inline-assembly
+    assembly {
+      sstore(slot, newStorage)
     }
+  }
 
-    function changeAdmin(address newAdmin) external ifAdmin {
-        require(newAdmin != address(0), "TransparentUpgradeableProxy: new admin is the zero address");
-        emit AdminChanged(_admin(), newAdmin);
-        _setAdmin(newAdmin);
-    }
-
-    function controllers() external view returns (address, address) {
-        return (_admin(), _governance());
-    }
-
-    function _admin() internal view returns (address adm) {
-        bytes32 slot = _ADMIN_SLOT;
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            adm := sload(slot)
-        }
-    }
-
-    function _governance() internal view returns (address gov) {
-        bytes32 slot = _GOVERNANCE_SLOT;
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            gov := sload(slot)
-        }
-    }
-
-    function _beforeFallback() internal {
-        require(!_isAdmin(msg.sender), "Admins can't play farming games");
-        super._beforeFallback();
-    }
-
-    function _setAdmin(address newAdmin) private {
-        bytes32 slot = _ADMIN_SLOT;
-
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            sstore(slot, newAdmin)
-        }
-    }
-
-    function _setGovernance(address newGovernance) private {
-        bytes32 slot = _GOVERNANCE_SLOT;
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            sstore(slot, newGovernance)
-        }
-    }
 }
--- contracts/proxies/TimeLockedProxy.sol
+++ contracts/proxies/TimeLockedProxy.sol
@@ -4,110 +4,110 @@ import "./GovernableProxy.sol";
 
 // Ensures that upgrades occur after a mandatory waiting period. One queued
 // update is stored and automatically activated at a specific timestamp
-contract TimelockedProxy is GovernableProxy {
+contract TimeLockedProxy is GovernableProxy {
 
-    // the number of seconds to delay a change
-    bytes32 private constant _TIMER_SLOT = 0x16ea8945dccfd60816131e877e35fb4c75542f840cf8432eafe6a82aec3fb393;
+  // the number of seconds to delay a change
+  bytes32 private constant _TIMER_SLOT = 0x16ea8945dccfd60816131e877e35fb4c75542f840cf8432eafe6a82aec3fb393;
 
-    // the new target at the change
-    bytes32 private constant _NEXT_SLOT = 0x8f58a16f4dc0b4ae97b4b89fc992033e37fc1602778ac3cb5502d7138bee1d0e;
+  // the new target at the change
+  bytes32 private constant _NEXT_SLOT = 0x8f58a16f4dc0b4ae97b4b89fc992033e37fc1602778ac3cb5502d7138bee1d0e;
 
-    // the timestamp when the change happens
-    bytes32 private constant _SCHEDULED_SLOT = 0x3889c1d17c017840e6a02a4d3c18713f27a6ff8fca4bcba0a7c4e42c9b49c6ec;
+  // the timestamp when the change happens
+  bytes32 private constant _SCHEDULED_SLOT = 0x3889c1d17c017840e6a02a4d3c18713f27a6ff8fca4bcba0a7c4e42c9b49c6ec;
 
-    constructor(address _logic, bytes memory _data, address _admin, address _governance, uint256 _timer)
-    GovernableProxy(_logic, _data, _admin, _governance)
-    public
-    {
-        assert(_TIMER_SLOT == bytes32(uint256(keccak256("eip1967.proxy.Things long Past")) - 1));
-        assert(_NEXT_SLOT == bytes32(uint256(keccak256("eip1967.proxy.Waiting in the Wings")) - 1));
-        assert(_SCHEDULED_SLOT == bytes32(uint256(keccak256("eip1967.proxy.Things yet to Come")) - 1));
-        _setTimer(_timer);
+  constructor(address _logic, bytes memory _data, address _admin, address _governance, uint256 _timer)
+  GovernableProxy(_logic, _data, _admin, _governance)
+  public
+  {
+    assert(_TIMER_SLOT == bytes32(uint256(keccak256("eip1967.proxy.Things long Past")) - 1));
+    assert(_NEXT_SLOT == bytes32(uint256(keccak256("eip1967.proxy.Waiting in the Wings")) - 1));
+    assert(_SCHEDULED_SLOT == bytes32(uint256(keccak256("eip1967.proxy.Things yet to Come")) - 1));
+    _setTimer(_timer);
+  }
+
+  function _setNext(address _nextImpl) private {
+    bytes32 slot = _NEXT_SLOT;
+
+    // solhint-disable-next-line no-inline-assembly
+    assembly {
+      sstore(slot, _nextImpl)
     }
+  }
 
-    function _setNext(address _nextImpl) private {
-        bytes32 slot = _NEXT_SLOT;
+  function _setTimer(uint256 _tim) private {
+    bytes32 slot = _TIMER_SLOT;
 
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            sstore(slot, _nextImpl)
-        }
+    // solhint-disable-next-line no-inline-assembly
+    assembly {
+      sstore(slot, _tim)
     }
+  }
 
-    function _setTimer(uint256 _tim) private {
-        bytes32 slot = _TIMER_SLOT;
+  function _setScheduled(uint256 _sched) private {
+    bytes32 slot = _SCHEDULED_SLOT;
 
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            sstore(slot, _tim)
-        }
+    // solhint-disable-next-line no-inline-assembly
+    assembly {
+      sstore(slot, _sched)
     }
+  }
 
-    function _setScheduled(uint256 _sched) private {
-        bytes32 slot = _SCHEDULED_SLOT;
-
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            sstore(slot, _sched)
-        }
+  function _timer() internal view returns (uint256 tim) {
+    bytes32 slot = _TIMER_SLOT;
+    // solhint-disable-next-line no-inline-assembly
+    assembly {
+      tim := sload(slot)
     }
+  }
 
-    function _timer() internal view returns (uint256 tim) {
-        bytes32 slot = _TIMER_SLOT;
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            tim := sload(slot)
-        }
+  function _scheduled() internal view returns (uint256 tim) {
+    bytes32 slot = _SCHEDULED_SLOT;
+    // solhint-disable-next-line no-inline-assembly
+    assembly {
+      tim := sload(slot)
     }
+  }
 
-    function _scheduled() internal view returns (uint256 tim) {
-        bytes32 slot = _SCHEDULED_SLOT;
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            tim := sload(slot)
-        }
+  function _next() internal view returns (address nxt) {
+    bytes32 slot = _NEXT_SLOT;
+    // solhint-disable-next-line no-inline-assembly
+    assembly {
+      nxt := sload(slot)
     }
+  }
 
-    function _next() internal view returns (address nxt) {
-        bytes32 slot = _NEXT_SLOT;
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            nxt := sload(slot)
-        }
+  function timer() external view returns (uint256) {
+    return _timer();
+  }
+
+  function scheduledChange() external view returns (uint256, address) {
+    return (_scheduled(), _next());
+  }
+
+  function setNext(address _nextImpl) ifAdmin external {
+    uint32 extSize;
+    assembly {
+      extSize := extcodesize(_nextImpl)
     }
+    require(extSize != 0, "TimelockedProxy: new implementation is not a contract");
 
-    function timer() external view returns (uint256) {
-        return _timer();
+    _setNext(_nextImpl);
+    _setScheduled(block.timestamp + _timer());
+  }
+
+  // main logic. if the timer has elapsed and there is a next,
+  // upgrade to it
+  function _beforeFallback() internal {
+    uint256 scheduled = _scheduled();
+    // break up the conditions to save a SLOAD on most calls
+    if (scheduled != 0 && block.timestamp > scheduled) {
+      address nextImpl = _next();
+      if (nextImpl != address(0)) {
+        _upgradeTo(nextImpl);
+        _setNext(address(0));
+      }
+      _setScheduled(0);
     }
-
-    function scheduledChange() external view returns (uint256, address) {
-        return (_scheduled(), _next());
-    }
-
-    function setNext(address _nextImpl) ifAdmin external {
-        uint32 extSize;
-        assembly {
-            extSize := extcodesize(_nextImpl)
-        }
-        require(extSize != 0, "TimelockedProxy: new implementation is not a contract");
-
-        _setNext(_nextImpl);
-        _setScheduled(block.timestamp + _timer());
-    }
-
-    // main logic. if the timer has elapsed and there is a next,
-    // upgrade to it
-    function _beforeFallback() internal {
-        uint256 scheduled = _scheduled();
-        // break up the conditions to save a SLOAD on most calls
-        if (scheduled != 0 && block.timestamp > scheduled) {
-            uint256 nextImpl = _next();
-            if (nextImpl != address(0)) {
-                _upgradeTo(nextImpl);
-                _setNext(address(0));
-                _setScheduled(0);
-            }
-        }
-        super._beforeFallback();
-    }
+    super._beforeFallback();
+  }
 }
